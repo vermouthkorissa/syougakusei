41950d497f0257887c981e5b9b7d5cd8f6275c22
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-05-21 15:41:58 +0900

    Initial Commit

---
+++ Kotowaza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kotowaza
+//
+//  Created by 鈴木理紗子 on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
