ee8d49a0cff240ead97762ccfcdb73a5994565d8
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-05-29 12:59:15 +0900

    set

---
+++ Kotowaza/AnimalCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  AnimalCollectionViewCell.swift
+//  Kotowaza
+//
+//  Created by 鈴木理紗子 on 2023/05/29.
+//
+
+import UIKit
+
+class AnimalCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Kotowaza/ViewController.swift
+++ Kotowaza/ViewController.swift
@@ -7,32 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController,
-UICollectionViewDelegate,
-                      UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 16
-    }
+class ViewController: UIViewController
+{
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-
-            //セル上のTag(1)とつけたUILabelを生成
-            let label = cell.contentView.viewWithTag(1) as! UILabel
-
-            //今回は簡易的にセルの番号をラベルのテキストに反映させる
-            label.text = String(indexPath.row + 1)
-
-            return cell
-    }
     
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    
+    
 }
-
